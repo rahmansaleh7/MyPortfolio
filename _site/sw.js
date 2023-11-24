fbea37be681502555ec61a9fb71924fe684183fe
Iqbal <rahman.saleh7@gmail.com>
2023-11-24 13:59:27 +0700

    google-site-verification

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700730499';
+const cacheName = 'chirpy-1700809140';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
